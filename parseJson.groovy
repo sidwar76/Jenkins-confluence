b4c68722e747ce0853d7a343942fce7e6b623b9e
sidwar76 <75777879+sidwar76@users.noreply.github.com>
2024-02-12 21:59:20 +0530

    Update parseJson.groovy

--- parseJson.groovy
+++ parseJson.groovy
@@ -1,12 +1,7 @@
 import groovy.json.JsonSlurper
 
 @NonCPS
-def parseJsonFile(String jsonFilePath) {
-    echo "Parsing JSON file at: ${jsonFilePath}"
-    def jsonContent = new File(jsonFilePath).text
-    echo "JSON content: ${jsonContent}"
+def parseJsonText(String jsonText) {
     def slurper = new JsonSlurper()
-    def parsedData = slurper.parseText(jsonContent)
-    echo "Parsed JSON data: ${parsedData}"
-    return parsedData
+    return slurper.parseText(jsonText)
 }
